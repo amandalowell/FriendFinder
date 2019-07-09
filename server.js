547be941f4286a32552d16ad0629dee38dbded33
Amanda Lowell <amandalowell2016@gmail.com>
2019-07-08 20:57:41 -0500

    app use

--- server.js
+++ server.js
@@ -8,5 +8,8 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 
-var apiroutes = require("./app/routing/apiRoutes.js");
-var htmlroutes = require("./app/routing/htmlRoutes.js");
+var apiRoutes = require("./app/routing/apiRoutes.js");
+var htmlRoutes = require("./app/routing/htmlRoutes.js");
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
