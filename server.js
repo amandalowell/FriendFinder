e251b637f9015e762a542e1c7f896ef3fde562c1
Amanda Lowell <amandalowell2016@gmail.com>
2019-07-08 20:54:21 -0500

    routes

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+
+var apiroutes = require("./app/routing/apiRoutes.js");
+var htmlroutes = require("./app/routing/htmlRoutes.js");
