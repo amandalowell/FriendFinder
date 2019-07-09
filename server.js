e5a320132d94be45eb0dc1efc38c8d6e101e8113
Amanda Lowell <amandalowell2016@gmail.com>
2019-07-08 21:10:09 -0500

    server finish

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ var path = require("path");
 
 
 var app = express();
+
 var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
 var apiRoutes = require("./app/routing/apiRoutes.js");
@@ -18,3 +22,7 @@ app.use(htmlRoutes);
 app.listen(PORT, function() {
     console.log("listening on PORT: " + PORT);
   });
+
+app.get("/", function(req,res) {
+    res.sendFile(path.join(__dirname, "app/public.html"));
+})
